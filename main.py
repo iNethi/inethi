993e93192005bf5e13dc68e4a7e4e076b0e35316
Keegan White <keeganthomaswhite@gmail.com>
2023-08-30 09:57:25 +0200

    updated main and added azuracast as a service

--- main.py
+++ main.py
@@ -41,8 +41,8 @@ def run_playbook(playbook_name, playbook_dir_path, inventory_path):
 
 def main():
     abs_path = pathlib.Path(__file__).parent.resolve()
-    choices = ['jellyfin', 'nextcloud', 'splash-screen', 'radiusdesk', 'wordpress']
-    # choices = ['jellyfin', 'nextcloud', 'splash-screen', 'radiusdesk', 'wordpress', 'maintain', 'keycloak']
+    choices = ['azuracast', 'jellyfin', 'keycloak', 'nextcloud', 'splash-screen', 'wordpress']
+    # choices = ['dnsmasq', 'jellyfin', 'keycloak', 'nextcloud', 'splash-screen', 'radiusdesk', 'wordpress']
     traefik = 'traefik'
     test_server = "test_server_connection"
     system_requirements = "system_requirements"
@@ -59,22 +59,36 @@ def main():
     write_to_inventory(ip, user, password, inventory_path)
     save_ip_to_config(ip, config_path)
 
-    # Run initial playbooks
-    run_playbook(test_server, playbook_dir_path, inventory_path)
-    run_playbook(system_requirements, playbook_dir_path, inventory_path)
-    run_playbook(traefik, playbook_dir_path, inventory_path)
+    skip_setup = input(
+        "Have you run the builder successfully before and want to skip system setup? (y/n): ").strip().lower()
+
+    # Run initial playbooks only if the user does not want to skip setup
+    if skip_setup != 'y':
+        run_playbook(test_server, playbook_dir_path, inventory_path)
+        run_playbook(system_requirements, playbook_dir_path, inventory_path)
+        run_playbook(traefik, playbook_dir_path, inventory_path)
 
     # Prompt user to select services to install
     print("Select services to install:")
     for idx, choice in enumerate(choices, 1):
         print(f"{idx}. {choice}")
 
-    selected_indexes = input("Enter the numbers of the services you want to install (comma-separated): ").split(',')
-    selected_services = [choices[int(idx) - 1] for idx in selected_indexes]
+    selected_indexes = input("Enter the numbers of the services you want to install then press enter ("
+                             "comma-separated... eg: 1,2,3): ").split(',')
 
-    # Run playbooks for selected services
-    for service in selected_services:
-        run_playbook(service, playbook_dir_path, inventory_path)
+    try:
+        selected_services = [choices[int(idx) - 1] for idx in selected_indexes]
+        for service in selected_services:
+
+            run_playbook(service, playbook_dir_path, inventory_path)
+    except Exception as e:
+        print("Unable to run installations... Did you enter your choices correctly?")
+        print("")
+        print("##########")
+        print("LOGS:")
+        print("The error that was caught: ", e)
+        print("##########")
+
 
 
 if __name__ == '__main__':
